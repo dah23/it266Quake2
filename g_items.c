4e474bff6e82aa91cb37ae64a6c979a3fd2cf999
dah23 <dah23@njit.edu>
2014-10-12 18:21:17 -0400

    press one to swap between sword/blaster

--- g_items.c
+++ g_items.c
@@ -17,6 +17,10 @@ void Weapon_GrenadeLauncher (edict_t *ent);
 void Weapon_Railgun (edict_t *ent);
 void Weapon_BFG (edict_t *ent);
 
+////////////
+void Weapon_Sword (edict_t *ent);
+///////////
+
 gitem_armor_t jacketarmor_info	= { 25,  50, .30, .00, ARMOR_JACKET};
 gitem_armor_t combatarmor_info	= { 50, 100, .60, .30, ARMOR_COMBAT};
 gitem_armor_t bodyarmor_info	= {100, 200, .80, .60, ARMOR_BODY};
@@ -1523,6 +1527,31 @@ always owned, never in the world
 	//
 	// AMMO ITEMS
 	//
+/*===========================*/
+/*weapon_sword*/
+
+		{	
+			"weapon_sword",
+				NULL,
+				Use_Weapon,
+				NULL,
+				Weapon_Sword,
+				"mis/w_pkup.wav",
+				NULL,
+				0,
+				"models/weapons/v_blast/tris.md2",
+				"w_blaster",
+				"Sword",
+				0,
+				0,
+				NULL,
+				IT_WEAPON,
+				NULL,
+				0,
+				"weapons/hgrenlb1b.wav misc/fhit3.wav"
+
+		},
+/*===========================*/
 
 /*QUAKED ammo_shells (.3 .3 1) (-16 -16 -16) (16 16 16)
 */
