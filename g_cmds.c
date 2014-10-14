587d743396c3b2b39d04d78b743f68d2d19453eb
dah23 <dah23@njit.edu>
2014-10-14 01:01:14 -0400

    pheeww for some reason emrging shield wiped it

--- g_cmds.c
+++ g_cmds.c
@@ -1000,13 +1000,13 @@ void ClientCommand (edict_t *ent)
 		Cmd_Wave_f (ent);
 	else if (Q_stricmp(cmd, "playerlist") == 0)
 		Cmd_PlayerList_f(ent);
-<<<<<<< HEAD
+
 	
-=======
+
 	 else if (Q_stricmp (cmd, "flash") == 0)
 		Cmd_FlashGrenade_f (ent);
 
->>>>>>> flashgrenades
+
 	else	// anything that doesn't match a command will be a chat
 		Cmd_Say_f (ent, false, true);
 }
